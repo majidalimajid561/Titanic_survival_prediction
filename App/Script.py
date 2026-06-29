eef68b7b10d932270a16283582366dab3ed81307
majidalimajid561 <majid.official561@gmail.com>
2026-06-29 16:56:02 +0500

    changing model  to Decesion tree

--- App/Script.py
+++ App/Script.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 @st.cache_resource
 def load_model():
-    return joblib.load('models/titanic_model.pkl')
+    return joblib.load('models/Titanic_model_DT.pkl')
 
 model = load_model()
 
@@ -25,33 +25,32 @@ st.divider()
 with st.form("passenger_form"):
     st.subheader("Passenger Information")
 
-    col1, col2 = st.columns(2)
-    with col1:
-        pclass = st.selectbox(
+    
+    pclass = st.selectbox(
             "Ticket Class (1 = 1st, 2 = 2nd, 3 = 3rd)",
             options=[1, 2, 3],
             help="1st class is the most expensive, 3rd is the cheapest. Higher class often had better survival chances."
         )
-        sex = st.radio(
+    sex = st.radio(
             "Sex",
             options=['male', 'female'],
             horizontal=True,
             help="Women and children were given priority on lifeboats."
         )
-        age=st.slider("Age(Year)", min_value=1, max_value=80,
+    age=st.slider("Age(Year)", min_value=1, max_value=80,
                       help="Age of the passenger. Children had higher survival rates.")
-    with col2:
-        family_size = st.number_input(
+    
+    family_size = st.number_input(
             "Family Size (including self)",
             min_value=1, max_value=10, value=1,
             help="Total number of family members aboard (siblings/spouse + parents/children + 1)."
         )
-        fare = st.slider(
+    fare = st.slider(
             "Fare (£)",
             min_value=0.0, max_value=500.0, value=32.0, step=0.5,
             help="Ticket fare in British pounds. Higher fares often meant better cabin locations."
         )
-        embarked = st.selectbox(
+    embarked = st.selectbox(
             "Port of Embarkation",
             options=['S', 'C', 'Q'],
             help="S = Southampton, C = Cherbourg, Q = Queenstown. The port where the passenger boarded."
